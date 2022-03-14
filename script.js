cfc18947b5eab16d1cc13c45e4aa485924d78d71
João Oneta <joaomatheusoneta01@gmail.com>
2022-03-14 19:35:35 -0300

    Alterações no estilo CSS da página para facilitar a testagem da aplicação, além da implementação da função Calcular que possibilita o funcionamento da calculadora.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function digitar(num){
-    document.getElementById("tela").value += num;
+function Calcular(num){
+    if(num == 'c'){
+        document.getElementById('tela').value = '';
+    }else if(num == '='){
+        let resultado = eval(document.getElementById('tela').value);
+        document.getElementById('tela').value = resultado;
+    }else{
+        document.getElementById('tela').value += num;
+    }
 }
